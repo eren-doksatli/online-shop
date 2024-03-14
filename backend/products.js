47f9dedbba884bf8e7ea716f83bdf277623a286b
eren-dox <eren.doksatli@gmail.com>
2024-03-14 12:04:44 +0300

    first commit

---
+++ backend/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 15 Pro",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/dgbhqtx5o/image/upload/v1706993131/onlineShop/iphone12pro_ibzqgg.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 15",
+    desc: "5.4-inch display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/dgbhqtx5o/image/upload/v1706993137/onlineShop/iphone12_elfj5z.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/dgbhqtx5o/image/upload/v1706993148/onlineShop/galaxyS_gg9jrs.png",
+  },
+];
+
+module.exports = products;
